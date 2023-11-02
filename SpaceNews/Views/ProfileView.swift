867ce56e5ca6874415dfc6e1eed4afcfb3d8c2a7
drfosho <kevingodbey1@gmail.com>
2023-11-02 09:27:48 -0700

    Updating SpaceNews to have a user profile page as well as ability to save articles

---
+++ SpaceNews/Views/ProfileView.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileView.swift
+//  SpaceNews
+//
+//  Created by Kevin Godbey on 11/1/23.
+//
+
+import Foundation
---
+++ SpaceNews/Views/UserDefaultsManager.swift
@@ -0,0 +1,8 @@
+//
+//  UserDefaultsManager.swift
+//  SpaceNews
+//
+//  Created by Kevin Godbey on 11/2/23.
+//
+
+import Foundation
